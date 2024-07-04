92e9d4299b2160c67ab9cfaf2a41b30cc162f65a
Sherif Jimoh <Sherefjimoh@gmail.com>
2024-07-04 10:40:28 +0100

    implement pagination to transaction history and recent transaction component

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,5 +1,6 @@
 import HeaderBox from '@/components/HeaderBox';
 import TransactionsTable from '@/components/TransactionsTable';
+import { Pagination } from '@/components/Pagination';
 
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import { getAccounts, getAccount } from '@/lib/actions/bank.actions';
@@ -18,7 +19,16 @@ const TransactionHistory = async ({
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
   const account = await getAccount({ appwriteItemId });
 
-  console.log({ 'wobi werey': account });
+  const rowsPerPage = 10;
+  const totalPages = Math.ceil(account?.transactions.length / rowsPerPage);
+
+  const indexOfLastTransaction = currentPage * rowsPerPage;
+  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+  const currentTransactions = account?.transactions.slice(
+    indexOfFirstTransaction,
+    indexOfLastTransaction
+  );
 
   return (
     <section className='transactions'>
@@ -39,7 +49,7 @@ const TransactionHistory = async ({
               {account?.data.officialName}
             </p>
             <p className='text-14 font-semibold tracking-[1.1px] text-white'>
-              ●●●● ●●●● ●●●● {' '}
+              ●●●● ●●●● ●●●●{' '}
               <span className='text-16'>{account?.data.mask}</span>
             </p>
           </div>
@@ -53,7 +63,12 @@ const TransactionHistory = async ({
         </div>
 
         <section className='flex w-full flex-col gap-6'>
-          <TransactionsTable transactions={account.transactions} />
+          <TransactionsTable transactions={currentTransactions} />
+          {totalPages > 1 && (
+            <div className='my-4 w-full'>
+              <Pagination page={currentPage} totalPages={totalPages} />
+            </div>
+          )}
         </section>
       </div>
     </section>
